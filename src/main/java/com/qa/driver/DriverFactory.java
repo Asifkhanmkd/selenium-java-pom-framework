23e0ce7be2f587812b176b5eaf9ccd13a6c5e39c
Asif <asifkhanmkd@yahoo.com>
2025-07-07 11:05:43 +0100

    Code updated to automaticallty select when running the tests in gitHub

--- src/main/java/com/qa/driver/DriverFactory.java
+++ src/main/java/com/qa/driver/DriverFactory.java
@@ -2,70 +2,80 @@ package com.qa.driver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
-	
-	private static ThreadLocal<WebDriver> tdriver= new ThreadLocal<WebDriver>();
-	
-	  public WebDriver initDriver(String browser) {
-		  
-		  
-		  switch(browser.toLowerCase()) {
-		  
-		  case"chrome":
-		  
-			  WebDriverManager.chromedriver().setup();
-			  tdriver.set(new ChromeDriver());
+
+	private static ThreadLocal<WebDriver> tdriver = new ThreadLocal<WebDriver>();
+
+	public WebDriver initDriver(String browser) {
+
+		switch (browser.toLowerCase()) {
+
+		case "chrome":
+
+			WebDriverManager.chromedriver().setup();
+			ChromeOptions cOption = new ChromeOptions();
+
+			if (System.getenv("CI") != null) {
+				cOption.addArguments("--headless=new", "--disable-gpu", "--window-size=1920,1080");
+			}
+			tdriver.set(new ChromeDriver(cOption));
+
+			break;
+
+		case "firefox":
+
+			WebDriverManager.firefoxdriver().setup();
+
+			FirefoxOptions fOption = new FirefoxOptions();
+			if (System.getenv("CI") != null) {
+
+				fOption.addArguments("--headless");
+			}
+
+			tdriver.set(new FirefoxDriver());
+
+			break;
+
+		case "edge":
+
+			WebDriverManager.edgedriver().setup();
+			EdgeOptions eOption= new EdgeOptions();
 			
-			  break;
-			  
-			  
-		  case "firefox":
-		  
-		   WebDriverManager.firefoxdriver().setup();
-		   tdriver.set(new FirefoxDriver());
-		   
-		   break;
-		   
-		   case "edge":
-		   
-		   WebDriverManager.edgedriver().setup();
-		   tdriver.set(new EdgeDriver());
-		  
-		   
-		   break;
-		   
-		  
-		  
-		 default:
-			   throw new IllegalArgumentException("unsupported browser: "+browser);
-	  }
-		
-		  return tdriver.get();
+			if(System.getenv("CI")!=null) {
+				eOption.addArguments("--headless=new","--disable-gpu","--window-size=1920,1080")
+				;
+			}
+			tdriver.set(new EdgeDriver());
 
-		  
-		  
-		  
+			break;
 
-}
-	  public static WebDriver getDriver() {
-		  
-		  return tdriver.get();
-	  }
-	  
-	  
-	  public static void quitDriver() {
-	        WebDriver driver = tdriver.get();
-	        if (driver != null) {
-	            driver.quit();
-	            tdriver.remove(); // Remove from ThreadLocal
-	        }
-	    }
-	  
-	 
-	    
+		default:
+			throw new IllegalArgumentException("unsupported browser: " + browser);
+		}
+
+		return tdriver.get();
+
+	}
+
+	public static WebDriver getDriver() {
+
+		return tdriver.get();
+	}
+
+	public static void quitDriver() {
+		WebDriver driver = tdriver.get();
+		if (driver != null) {
+			driver.quit();
+			tdriver.remove(); // Remove from ThreadLocal
+		}
+	}
+
 }
